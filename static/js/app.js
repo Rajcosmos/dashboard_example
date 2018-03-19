59db93bdea79ab521ee5b1ba8c9cca7891434329
Rajcosmos <rajat_kaura@yahoo.com>
2018-03-18 18:03:36 -0700

    updateing files..

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,25 @@
 
-
+//**  Collect Data from /names extract "response" and on change trigger getdata function for the selected drop down value) */
 function option_selection(){
+
 var queryURL="/names";
+
 d3.json(queryURL, function(error, response) {
+
     console.log('error:', error);
     if (error) return console.warn(error);
-    console.log(response);
-    console.log("type", typeof(response));
-   // document.write(JSON.stringify(response));
-
+    //console.log(response);
+    //console.log("type", typeof(response));
     for (i=0 ; i< response.length; i++){
       var names_options = response[i];
-     // console.log(names_options);
+     //using d3 to feed the drop in data so that we can select the options
       d3.select('#selDataset')
         .append('option')
         .on("change",optionChanged)
         .attr('value',response[i])
         .text(response[i]);
-
+     //Trigger function on change
       function optionChanged(){
-      
        var data=d3.select('option').node().value;
        getData(data)
       }
@@ -40,18 +40,93 @@ function init_pie() {
     },
   ];
   var layout = {
-    height: 600,
-    width: 800,
+    height: 400,
+    width: 400,
   };
   Plotly.plot('pie', data, layout);
 }
-function updatePlotly(newdata,labels) {
+
+
+function updatePlotly_pie(newdata,labels) {
   var PIE = document.getElementById('pie');
   console.log(labels);
-  console.log(newdata);
+  //console.log(newdata);
   Plotly.restyle(PIE, 'values', [newdata]);
   Plotly.restyle(PIE, 'labels', [labels]);
 }
+
+//** Gauge Chart *****************
+
+function init_gauge()
+{
+// Enter a speed between 0 and 180
+var level = 175;
+
+// Trig to calc meter point
+var degrees = 180 - level,
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var data = [{ type: 'scatter',
+   x: [0], y:[0],
+    marker: {size: 28, color:'850000'},
+    showlegend: false,
+    name: 'speed',
+    text: level,
+    hoverinfo: 'text+name'},
+  { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+  rotation: 90,
+  text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
+            'Slow', 'Super Slow', ''],
+  textinfo: 'text',
+  textposition:'inside',      
+  marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                         'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                         'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                         'rgba(255, 255, 255, 0)']},
+  labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+}];
+
+var layout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+  title: '<b>Gauge</b> <br> Speed 0-100',
+  height: 400,
+  width: 400,
+  xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('gauge', data, layout);
+
+
+
+}
+
+
+
 
 // Bubble plot *******
 
@@ -75,17 +150,17 @@ function init_bubble() {
      var layout={
              title:'Hey!, Bubble Chart...',
              showlegend:false,
-             height:600,
-             width:1000
+             height:500,
+             width:900,
      };
      Plotly.newPlot('bubble',data,layout);
     }
     
-     function updatePlotly(y,x) {
+     function updatePlotly_bubble(y,x) {
           var BUBBLE = document.getElementById('bubble');
        
            console.log(x);
-           console.log(y);
+          // console.log(y);
             var s =[];
               s=y;
     
@@ -98,48 +173,61 @@ function init_bubble() {
            Plotly.restyle(BUBBLE, 'x', [x]);
            Plotly.restyle(BUBBLE, 'y', [y]);
            Plotly.restyle(BUBBLE, 'marker.size',[y]);
-
-          // Plotly.restyle(BUBBLE,'color',[y]);
-           
+          // Plotly.restyle(BUBBLE,'color',[y]);     
       }
 
 
 
 function getData(data) {
-  console.log(data);
-  var url="/samples/"+data;
 
-  console.log(url)
+  // Data for Pie chart - otuid and sample_values
+  var url="/samples/"+data;
+  //Otuid description
+  var url_desc="/otu";
+ //console.log(url)
+
+// assigning samplevalue and otuid as arrays
   var samplevalues = [];
   var otuid = [];
+
+  // Get response from the /samples/<sample>
   d3.json(url, function(error, response) {
     //console.log('error:', error);
     //if (error) return console.warn(error);
-    console.log(response);
-    console.log("type", typeof(response));
-    //console.log(JSON.stringify(response));
-
- 
-    for (i=1; i<11;i++){
+    //console.log(response);
+    //console.log("type", typeof(response));
+    //console.log(response[0].otu_ids.length);
+    //console.log(response[0].sample_values.length);
+    
+    for (var i=1; i<response[0].otu_ids.length;i++){
 
       samplevalues.push(response[0].sample_values[i]);
       otuid.push(response[0].otu_ids[i]);
-      
-      
     }
 
-console.log(samplevalues);
-console.log(otuid);
+   // console.log(samplevalues);
+   // console.log(otuid);
+//NOTE : tried sorting but it seems the values of otuid remains same hence picking only top 10 sample values**
+    samplevalues.sort(function(a,b){return b - a});
+    //otuid.sort(function(a,b){return b - a});
+    //console.log("After sort")
+//console.log(samplevalues);
+//console.log(otuid);
+ 
+var dataPlots=[]
+var labelPlots=[]
+ for (var i=0; i<10; i++) {
+ dataPlots.push(samplevalues[i]);
+ labelPlots.push(otuid[i]);
+ }
+updatePlotly_pie(dataPlots,labelPlots);  
+updatePlotly_bubble(dataPlots,labelPlots);
 
-
-updatePlotly(samplevalues,otuid);  
-    //samplevalues.sort(function(a,b){return b - a});
-    //console.log(response.sample_values[0]);
 })
 }
 
-init_pie();
-
-init_bubble();
 
 option_selection();
+init_pie();
+init_gauge();
+init_bubble();
